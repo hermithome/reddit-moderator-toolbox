45e18775738b66433cb3c950c992c9c5e532bfb0
hermithome <hermithome@users.noreply.github.com>
2015-01-15 18:06:36 -0800

    Update notifier.js

--- modules/notifier.js
+++ modules/notifier.js
@@ -45,26 +45,26 @@ self.register_setting('checkInterval', {
 
 Notification settings:
 
-Notifications let you know what's new in your counter. They include the username and the subreddit of the new item. Depending on your browser and preferences, you may need to allow notifications for reddit. Clicking on a notification opens a new tab(or window, depending on your browser preferences)??? and takes you to the relevant page.  
+Notifications let you know what's new in your counter. They include the username and the subreddit of the new item. Depending on your browser and preferences, you may need to allow notifications for reddit. Clicking on a notification opens a new tab(or window, depending on your browser preferences) and takes you to the relevant page.  
 
 Personal Messages:
 
 self.register_setting('messageNotifications', {
     'type': 'boolean',
     'default': true,
-    'title': 'Get personal message notifications (comments and PMs). Clicking a message notification opens a new tab to the permalink for the comment/PM (including context if available) and marks it as read.'
+    'title': 'Get message notifications (comments and PMs). Clicking a message notification opens a new tab to the permalink for the comment/PM (including context if available) and marks it as read.'
 });
 
 self.register_setting('straightToInbox', {
     'type': 'boolean',
     'default': false,
-    'title': 'Clicking a personal message notification opens a new tab to your inbox instead of to the comment/PM permalink. When clicking the notification, toolbox does not mark the message as read, but depending on your reddit preferences, going to your inbox may mark your messages as read anyway.'
+    'title': 'Message notifications link to your inbox instead of to the comment/PM permalink. When clicking the notification, toolbox does not mark the message as read, but depending on your reddit preferences, going to your inbox may mark your messages as read anyway.'
 });
 
 self.register_setting('messageUnreadLink', {
     'type': 'boolean',
     'default': false,
-    'title': 'Link to /message/unread/ (instead of /message/inbox) if unread messages are present. Affects both the counter and notification pop-up.'
+    'title': 'Link to /message/unread/ (instead of /message/inbox) if unread messages are present. Affects both the counter and notification pop-ups.'
 });
 
 Modmail:
@@ -78,7 +78,7 @@ self.register_setting('modmailNotifications', {
 self.register_setting('modmailUnreadLink', {
     'type': 'boolean',
     'default': false,
-    'title': 'Link to /message/moderator/unread/ if unread modmail is present. Affects both counter and notification pop-up.'
+    'title': 'Link to /message/moderator/unread/ if unread modmail is present. Affects both the counter and notification pop-ups.'
 });
 
 Queues:
@@ -98,7 +98,7 @@ self.register_setting('unmoderatedNotifications', {
 self.register_setting('consolidatedMessages', {
     'type': 'boolean',
     'default': true,
-    'title': 'Consolidate notifications by type (ex: 5 new modquue items) instead of receiving individual notifications for each item. Clicking a consolidated message notification opens a new tab to /inbox (or /unread, depending on your preferences), and does not mark your messages as read. Clicking a consolidated moderator notification functions in the same way as clicking an individual moderator notification.
+    'title': 'Consolidate notifications by type (ex: 5 new modquue items) instead of receiving individual notifications for each item. Clicking a consolidated message notification opens a new tab to /inbox (or /unread, depending on your preferences), and does not mark your messages as read. Consolidated moderator notifications function just as individual moderator notifications.
 });
 
 
